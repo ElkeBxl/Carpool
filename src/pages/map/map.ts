645b4d1c6d19ebb7e8bc49e1daa893714d9080d2
Elke Heymans <Elke.Heymans@ordina.be>
2018-03-30 11:24:23 +0200

    Added marker to the map for Ordina, via search with the Google Maps API

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -23,14 +23,30 @@ export class MapPage {
   loadMap(){ 
     // Ordina coordinates
     let latLng = new google.maps.LatLng(51.0517983,4.4519565);
+
+    // Try to search for Ordina
+    var geocoder = new google.maps.Geocoder();
+    geocoder.geocode({'address': "Ordina Belgium"}, (results, status) => {
+      if (status === 'OK') {
+        latLng = results[0].geometry.location;
+        var marker = new google.maps.Marker({
+          position: latLng,
+          map: this.map
+        });
+        this.map.setCenter(latLng);
+      } else {
+        console.log("Failed to find address");
+      }
+    });
  
+    // Create the actual map
     let mapOptions = {
       center: latLng,
       zoom: 15,
       mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
- 
+    }; 
     this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions); 
+
   }
   
 }
