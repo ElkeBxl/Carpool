a5611f8189903cd54fbf3b81246cee03fd594ef8
Elke Heymans <Elke.Heymans@ordina.be>
2018-03-30 11:10:17 +0200

    Add participant button added with basic add into an array

--- src/pages/participants/participants.ts
+++ src/pages/participants/participants.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-participants',
@@ -7,8 +7,42 @@ import { NavController } from 'ionic-angular';
 })
 export class ParticipantsPage {
 
-  constructor(public navCtrl: NavController) {
+  participants: any[];
 
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
+    this.participants = [];
+    this.participants.push({ name: "Elke"});
+    this.participants.push({ name: "Ines"});
+  }
+
+  addParticipant() {
+    let prompt = this.alertCtrl.create({
+      title: 'New participant',
+      message: "Enter a name and address for this new participant",
+      inputs: [
+        {
+          name: 'name',
+          placeholder: 'Nane'
+        },
+        {
+          name: 'address',
+          placeholder: 'Address'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: data => { this.participants.push(data); } 
+        }
+      ]
+    });
+    prompt.present();
   }
 
 }
